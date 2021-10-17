ab65924de82f11df26d5429419b91ff601c848e1
pengfei <liu.pengfei@hotmail.fr>
2021-10-17 14:38:27 +0200

    update word count example

--- src/main/java/org.pengfei.example/WordCount.scala
+++ src/main/java/org.pengfei.example/WordCount.scala
@@ -2,6 +2,7 @@ package org.pengfei.example
 
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.rdd.{MapPartitionsRDD, ParallelCollectionRDD, RDD, ShuffledRDD}
 
 object WordCount {
 
@@ -13,7 +14,9 @@ object WordCount {
 
     // step2: create data
     val data=Seq("hadoop spark","hadoop flume","spark kafka")
-    val textRdd=spark.sparkContext.parallelize(data)
+    val  textRdd:RDD[String] = spark.sparkContext.parallelize(data)
+    // we can also read data from file
+    // val fileTextRdd=spark.sparkContext.textFile("")
 
     // step3: process data
     // split data by using " "
@@ -21,14 +24,14 @@ object WordCount {
     // val splitRdd=textRdd.flatMap(item=>item.split())
     // short version, when we only have one arg in the lamda expression. we can replace by it by _
     // in another word item=>item is replaced by _ (this is a scala shortcut)
-    val splitRdd=textRdd.flatMap(_.split(" "))
+    val splitRdd:RDD[String]=textRdd.flatMap(_.split(" "))
 
     // assign a init counter to each word,
     // long version
     // val tupleRdd=splitRdd.map(item=>(item,1))
     // short version
     // similar to above shortcut, we can replace item=> (item,1) by (_,1)
-    val tupleRdd=splitRdd.map((_,1))
+    val tupleRdd:RDD[(String,Int)]=splitRdd.map((_,1))
 
     // aggregate the word count
     // long version, when we apply reduceByKey on a Rdd, it will group all Rdd that have the same key, the accumulate
@@ -37,12 +40,12 @@ object WordCount {
     // we can also shortcut this by using _+_
     // val reduceRdd=tupleRdd.reduceByKey((current,accumulator)=>current+accumulator)
     // here the first _ presents current, the second presents accumulator
-    val reduceRdd=tupleRdd.reduceByKey(_+_)
+    val reduceRdd:RDD[(String,Int)]=tupleRdd.reduceByKey(_+_)
 
     // step4: get result, here we need to convert the result rdd to string
-    // the result rdd is like an array of (key, value), key is word, value is the wordcount
+    // the result rdd is like an array of (key, value), key is word, value is the word count
     // here we can't use short cut, because we don't have _ in the input as item.
-    val strRdd=reduceRdd.map(item=>s"${item._1}, ${item._2}")
+    val strRdd:RDD[String]=reduceRdd.map(item=>s"${item._1}, ${item._2}")
 
     // collect rdd to the driver
     strRdd.collect().foreach(item=>println(item))
